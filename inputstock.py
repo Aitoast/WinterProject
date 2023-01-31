a41b268f194e10f427e0e94050f5feadea2fb98d
이원찬 <105588857+Leewonchan14@users.noreply.github.com>
2023-02-01 03:01:43 +0900

    각자 sqlData.json에서 받아오는 정보로 변경

--- inputstock.py
+++ inputstock.py
@@ -2,13 +2,19 @@ import mojito  #한국투자증권에서 필요한 모듈
 import pandas as pd #pands 사용
 from sqlalchemy import create_engine
 import sys #js의 인자를 받기 위한 모듈
+import json #각자의 DB정보를 딕셔너리로 가져오는 json모듈
 
-f = open("Mykey.txt")  #text 파일을 만들고appkey,secretkey,계좌번호를 저장한다.
+
+f = open("Mykey.txt")  #text 파일을 열고 appkey,secretkey,계좌번호를 저장한다.
 lines = f.readlines()
 key = lines[0].strip()
 secret = lines[1].strip()
 acc_no = lines[2].strip()
 f.close()
+
+#solo_data변수에 딕셔너리로 담기
+with open('./SoloData/sqlData.json') as f:
+  solo_data = json.load(f)
 
 broker = mojito.KoreaInvestment(  #브로커객체를 생성
 	api_key=key,
@@ -20,7 +26,7 @@ response = broker.fetch_price(sys.argv[1]) #server.js 69번째 줄의 인자를 
 
 res=pd.DataFrame(response)
 
-db_connection_str ='mysql+pymysql://root:1234@localhost/solodb?charset=utf8'
+db_connection_str =f'mysql+pymysql://{solo_data.user}:{solo_data.password}@{solo_data.host}/{solo_data.database}?charset=utf8'
 db_connection = create_engine(db_connection_str)
 conn = db_connection.connect()
 
--- wholedata.py
+++ wholedata.py
@@ -1,6 +1,7 @@
 import mojito  #한국투자증권에서 필요한 모듈
 import pandas as pd 
 from sqlalchemy import create_engine
+import json #각자의 DB정보를 딕셔너리로 가져오는 json모듈
 
 f = open("Mykey.txt")  #text 파일을 만들고appkey,secretkey,계좌번호를 저장한다.
 lines = f.readlines()
@@ -18,10 +19,15 @@ broker = mojito.KoreaInvestment(  #브로커객체를 생성(broker.broker파일
 symbols = broker.fetch_symbols() # 전체 종목코드를 가져와 테이블에 넣는다. 
 sym=pd.DataFrame(symbols)
 
+#solo_data변수에 딕셔너리로 담기
+with open('./SoloData/sqlData.json') as f:
+  solo_data = json.load(f)
 
-db_connection_str ='mysql+pymysql://root:1234@localhost/solodb?charset=utf8'#파이썬과 mysql 연동 코드
+
+db_connection_str =f'mysql+pymysql://{solo_data.user}:{solo_data.password}@{solo_data.host}/{solo_data.database}?charset=utf8'
 db_connection = create_engine(db_connection_str)
 conn = db_connection.connect()
 
 sym.to_sql(name='hey',con=db_connection,if_exists='replace', index=False)# pandas dataframe으로 받아온 데이터를 mysql에 저장 
 
+conn.close()
