92d1734ed78b539deb86f7cc43401f1a43dc87dc
이원찬 <105588857+Leewonchan14@users.noreply.github.com>
2023-02-02 21:31:00 +0900

    axios 체험해봤음

--- wonchantest.js
+++ wonchantest.js
@@ -7,3 +7,29 @@
 // var ana = function(prams){
 //   console.log(`${1}`);
 // }
+
+// ana()
+
+import axios from 'axios'
+import fs from 'fs'
+
+var app = fs.readFileSync('./app.txt','utf8')
+var secret = fs.readFileSync('./secret.txt','utf8')
+
+var option = {
+  method: 'POST',
+  url: `https://openapi.koreainvestment.com:9443/oauth2/tokenP`,
+  data : JSON.stringify({ 
+    "grant_type": "client_credentials", 
+    "appkey" : app, 
+    "appsecret": secret, 
+  })
+}
+
+axios(option)
+.then(function (response) {
+  console.log(response.body);
+})
+.catch(function (error) {
+  console.log(error);
+});
