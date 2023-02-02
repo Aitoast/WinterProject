8e7136e7b5ca6a96f6a8e85eb9fcdb2b67a2eeb0
이원찬 <105588857+Leewonchan14@users.noreply.github.com>
2023-02-02 21:30:52 +0900

    원찬 테스트용

---
+++ wonchantest.js
@@ -0,0 +1,9 @@
+// import broker from './Mymojito/Mymojito.js'
+
+// broker.fetch_price('001500').then((body)=>{
+//   console.log(body);
+// })
+
+// var ana = function(prams){
+//   console.log(`${1}`);
+// }
