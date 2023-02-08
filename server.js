a1fc785d6201eaea65953e2646873ba31fe6a225
이원찬 <105588857+Leewonchan14@users.noreply.github.com>
2023-02-08 15:02:12 +0900

    간단한 if문 수정

--- server.js
+++ server.js
@@ -35,33 +35,26 @@ server.post("/", (req, res) => {
 
   connection.connect((err) => {
     if (err) console.log(err);
-    console.log("Connected successfully");
+    else console.log("Connected successfully");
   });
 
   //입력정보로 종목코드를 뽑아오는 sql 명령문
   let sql = `SELECT 단축코드 FROM solodb.stock WHERE 한글명="${req.body.stock}";`;
   connection.query(sql, function (err, stockcode) {
-    if (err) {
-      console.log(err);
-    } else {
+    if (err) console.log(err);
+    else {
       console.log(stockcode[0]["단축코드"]);
       broker.fetch_price(stockcode[0]["단축코드"]).then(function (stocka) {
         // 뽑아온 정보를 1행 테이블로 sql에 저장
         let del = `DELETE FROM solodb.stockinfo; `; // 원래 테이블을 초기화 시키는 명령문
         connection.query(del, function (err, results) {
-          if (err) {
-            console.log(err);
-          } else {
-            console.log("delete succesfully");
-          }
+          if (err) console.log(err);
+          else console.log("delete succesfully");
         });
         let savein = `INSERT INTO solodb.stockinfo SET ?; `; // 주식 정보를 table에 넣는 명령문
         connection.query(savein, stocka, function (err, results) {
-          if (err) {
-            console.log(err);
-          } else {
-            console.log("save succesfully");
-          }
+          if (err) console.log(err); 
+          else console.log("save succesfully");
         });
       });
       broker
@@ -79,11 +72,8 @@ server.post("/", (req, res) => {
           }
           var del2 = `DELETE FROM solodb.chartdata;`; //원래 테이블을 초기화 해주는 명령문
           connection.query(del2, function (err, results) {
-            if (err) {
-              console.log(err);
-            } else {
-              console.log("delete succesfully");
-            }
+            if (err) console.log(err);
+            else console.log("delete succesfully");
           });
           //주식 일 분봉을 시간까지 정해서 db에 데이터 저장
           var cha = `INSERT INTO solodb.chartdata(stck_bsop_date, stck_cntg_hour, stck_prpr, stck_oprc, stck_hgpr, stck_lwpr, cntg_vol, acml_tr_pbmn) values ?;`;
