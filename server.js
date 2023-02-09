b87d3de10e97b2960404e25c9d6999951fe6c783
이원찬 <105588857+Leewonchan14@users.noreply.github.com>
2023-02-09 17:36:03 +0900

    sql 문자열 변수들 주석과함께 변수명 수정

--- server.js
+++ server.js
@@ -38,15 +38,18 @@ server.post("/", (req, res) => {
     else console.log("Connected successfully");
   });
 
-  //입력정보로 종목코드를 뽑아오는 sql 명령문
-  let sql = `SELECT 단축코드 FROM solodb.stock WHERE 한글명="${stock_kr_string}";`;
-  connection.query(sql, function (err, respone) {
+  /**입력정보로 종목코드를 뽑아오는 sql 명령문*/
+  let select_sql = `SELECT 단축코드 FROM stock WHERE 한글명="${stock_kr_string}";`;
+  connection.query(select_sql, function (err, respone) {
+    //종목목록에 없는 종목을 사용자가 입력했을때
     if (err) console.log(err);
     else {
       /** 종목코드 */
       const stock_code = respone[0]["단축코드"];
       console.log(stock_code);
-      broker.fetch_price(stock_code).then(function (stocka) {
+      broker.fetch_price(stock_code).then(
+        /** @param {Object} stock_info fetch_price로 가져온 Object형 변수*/
+        function (stock_info) {
         // 여러 사용자가 데이터를 입력할 때 한 테이블에 쓰면 충돌이 발생할 것이기 때문에
         // 주식마다 주식정보, 분봉 테이블을 생성시킨다. 존재할 시 데이터 바로 입력
         let make = `CREATE TABLE solodb.${stock_kr_string}info ( 
@@ -132,14 +135,16 @@ server.post("/", (req, res) => {
           );`;
         connection.query(make, function (err, results) {
           if (err) {
-            let del = `DELETE FROM solodb.${stock_kr_string}info; `; // 원래 테이블을 초기화 시키는 명령문
-            connection.query(del, function (err, results) {
+            // 원래 테이블을 초기화 시키는 명령문
+            let del_sql = `DELETE FROM ${stock_kr_string}info; `; 
+            connection.query(del_sql, function (err, results) {
               if (err) console.log(err);
               else console.log("delete succesfully");
             });
-          } else console.log("create table succesfully");
-            let savein = `INSERT INTO solodb.${stock_kr_string}info SET ?;`;
-            connection.query(savein, stocka, function (err, results) {
+            } else console.log("create table succesfully");
+            //테이블 생성,초기화 이후 데이터 저장
+            let insert_sql = `INSERT INTO solodb.${stock_kr_string}info SET ?;`;
+            connection.query(insert_sql, stock_info, function (err, results) {
               if (err) console.log(err);
               else console.log("save succesfully");
             });
@@ -148,10 +153,11 @@ server.post("/", (req, res) => {
 
       broker
         .fetch_today_1m_ohlcv(stock_code, "")
-        .then(function (mindata) {
+        .then(
+          /** @param {Array<Array<string>>} mindata 분봉데이터 2차원배열*/
+          function (mindata) {
           // 데이터는 2차원배열로 받아 b에 넣는다.
-          var b = mindata;
-          var makemin = `CREATE TABLE solodb.${stock_kr_string}분봉( 
+          var create_sql = `CREATE TABLE solodb.${stock_kr_string}분봉( 
             stck_bsop_date VARCHAR(50),
               stck_cntg_hour VARCHAR(50),
               stck_prpr VARCHAR(50),
@@ -161,22 +167,26 @@ server.post("/", (req, res) => {
               cntg_vol VARCHAR(50),
               acml_tr_pbmn VARCHAR(50)
             );`;
-          connection.query(makemin, function (err, results) {
+          connection.query(create_sql, function (err, results) {
+            //create문이 에러라면 테이블 초기화
             if (err) {
-              let del = `DELETE FROM solodb.${stock_kr_string}분봉; `; // 원래 테이블을 초기화 시키는 명령문
-              connection.query(del, function (err, results) {
+              // 원래 테이블을 초기화 시키는 명령문
+              let del_sql = `DELETE FROM ${stock_kr_string}분봉; `; 
+              connection.query(del_sql, function (err, results) {
                 if (err) console.log(err);
                 else console.log("delete succesfully");
               });
             } else console.log("create table succesfully");
-            var cha = `INSERT INTO ${stock_kr_string}분봉 values ?;`;
-            connection.query(cha, [b], function (err, results) {
+            //테이블 생성,초기화 이후 데이터 저장
+            var insert_sql = `INSERT INTO ${stock_kr_string}분봉 values ?;`;
+            //주식 일 분봉을 시간까지 정해서 db에 데이터 저장
+            connection.query(insert_sql, [mindata], function (err, results) {
               if (err) console.log(err);
               else console.log("save succesfully");
               connection.end();
             });
           });
         });
-      }
-    });    
+    }
   });
+});
