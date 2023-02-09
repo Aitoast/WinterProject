70bfa9645a90b9656c016c3fe4f92b3dc890d0a3
이원찬 <105588857+Leewonchan14@users.noreply.github.com>
2023-02-09 18:01:13 +0900

    alt + shift + f 로 server.js 정리

--- server.js
+++ server.js
@@ -14,8 +14,8 @@ const stock_1m_columns = [
   "stck_hgpr",
   "stck_lwpr",
   "cntg_vol",
-  "acml_tr_pbmn"
-]
+  "acml_tr_pbmn",
+];
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -61,21 +61,21 @@ server.post("/", (req, res) => {
       broker.fetch_price(stock_code).then(
         /** @param {Object} stock_info fetch_price로 가져온 Object형 변수*/
         function (stock_info) {
-        // 여러 사용자가 데이터를 입력할 때 한 테이블에 쓰면 충돌이 발생할 것이기 때문에
-        // 주식마다 주식정보, 분봉 테이블을 생성시킨다. 존재할 시 데이터 바로 입력
-        let create_sql = `CREATE TABLE ${stock_kr_string}info ( 
+          // 여러 사용자가 데이터를 입력할 때 한 테이블에 쓰면 충돌이 발생할 것이기 때문에
+          // 주식마다 주식정보, 분봉 테이블을 생성시킨다. 존재할 시 데이터 바로 입력
+          let create_sql = `CREATE TABLE ${stock_kr_string}info ( 
           ${Object.keys(stock_info).join(` VARCHAR(50),
           `)} VARCHAR(50)
           );`;
-        connection.query(create_sql, function (err, results) {
-          //만약 create문이 에러라면 테이블을 초기화(삭제)한다.
-          if (err) {
-            // 원래 테이블을 초기화 시키는 명령문
-            let del_sql = `DELETE FROM ${stock_kr_string}info; `; 
-            connection.query(del_sql, function (err, results) {
-              if (err) console.log(err);
-              else console.log("delete succesfully");
-            });
+          connection.query(create_sql, function (err, results) {
+            //만약 create문이 에러라면 테이블을 초기화(삭제)한다.
+            if (err) {
+              // 원래 테이블을 초기화 시키는 명령문
+              let del_sql = `DELETE FROM ${stock_kr_string}info; `;
+              connection.query(del_sql, function (err, results) {
+                if (err) console.log(err);
+                else console.log("delete succesfully");
+              });
             } else console.log("create table succesfully");
             //테이블 생성,초기화 이후 데이터 저장
             let insert_sql = `INSERT INTO ${stock_kr_string}info SET ?;`;
@@ -83,8 +83,9 @@ server.post("/", (req, res) => {
               if (err) console.log(err);
               else console.log("save succesfully");
             });
-        });
-      });
+          });
+        }
+      );
 
       broker.fetch_today_1m_ohlcv(stock_code, "").then(
         /** @param {Array<Array<string>>} mindata 분봉데이터 2차원배열*/
@@ -97,7 +98,7 @@ server.post("/", (req, res) => {
             //create문이 에러라면 테이블 초기화
             if (err) {
               // 원래 테이블을 초기화 시키는 명령문
-              let del_sql = `DELETE FROM ${stock_kr_string}분봉; `; 
+              let del_sql = `DELETE FROM ${stock_kr_string}분봉; `;
               connection.query(del_sql, function (err, results) {
                 if (err) console.log(err);
                 else console.log("delete succesfully");
@@ -112,7 +113,8 @@ server.post("/", (req, res) => {
               connection.end();
             });
           });
-        });
+        }
+      );
     }
   });
 });
