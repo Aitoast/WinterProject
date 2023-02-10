5eab5e605c7649308051dbf9db47ab7f47b2bf6d
이원찬 <105588857+Leewonchan14@users.noreply.github.com>
2023-02-10 16:09:22 +0900

    html 템플릿 생성

--- 간단한html띄우는서버/server2.js
+++ 간단한html띄우는서버/server2.js
@@ -5,6 +5,20 @@ const port = 3000
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+//html 템플릿 생성
+var html = {
+  //홈페이지는 css, script를 읽은뒤 html 문자열을 반환함
+  hompage : function(css,script){
+    return `<style>
+    ${css}
+  </style>
+  <script src="https://code.highcharts.com/highcharts.js"></script>
+  <div id="container"></div>
+  <script>${script}</script>`
+  }
+}
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
